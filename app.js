0eebcf2837eaa29e31756f54a6224cfe9797cfd7
kelechi prosper <prosperkelechi50@gmail.com>
2021-10-22 10:24:16 +0100

    i

--- app.js
+++ app.js
@@ -4,3 +4,31 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// Load all event listener
+loadEventListeners();
+
+function loadEventListeners() {
+    // Add task event
+    form.addEventListeners('submit', addTask);
+
+}
+
+// Add Task
+function addTask(e) {
+    if(taskInput.value === '') {
+        alert('Add a task');
+    }
+
+    // create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    //create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // create new link element
+
+
+
+    e.preventDefault();
+}
